8d951fb3e2fb9019995e8764858b20f07bc479d8
Dennis Lee <leedennis04@gmail.com>
2023-11-06 19:57:38 -0600

    updated server.py

--- server.py
+++ server.py
@@ -55,9 +55,9 @@ def submit_form2(data="Lets see if you have a Strong Password!"):
         try:
             data = request.form.to_dict()
             pwd= str(data['Password To Check'])
-            pwned_api_check(pwd)
+            count = pwned_api_check(pwd)
             #return {data}
-            return render_template('workone.html', data=pwd)
+            return render_template('workone.html', data=count)
         except:
             return 'did not check against the database'
     else:
