0be1bc8f0edbd0993929201f43f5340a48f53dd4
Dennis Lee <leedennis04@gmail.com>
2023-11-14 18:19:19 -0600

    updated workone.html

--- server.py
+++ server.py
@@ -82,13 +82,13 @@ def submit_form2(data="Lets see if you have a Strong Password!"):
             data = request.form.to_dict()
             pwd= str(data['Password To Check'])
             count = pwned_api_check(pwd)
-            char = "*"
-            blockedpwd = char * (length(pwd) - 1)
             #return {data}
             if (count):
-                 return render_template('workone.html', data=f"Your password: \'{pwd[0]}{blockedpwd}\' was FOUND: {count} times! You should change your password!")
+                 # return render_template('workone.html', data=f"Your password: \'{pwd[0]}\' was FOUND: {count} times! You should change your password!")
+                return render_template('workone.html', data=f"Your password: was FOUND: {count} times! You should change your password!")
             else:
-                 return render_template('workone.html', data=f"Your password: \'{pwd[0]}{blockedpwd}\' was NOT FOUND. Carry on!")
+                 # return render_template('workone.html', data=f"Your password: \'{pwd[0]}\' was NOT FOUND. Carry on!")
+                return render_template('workone.html', data=f"Your password: was NOT FOUND. Carry on!")
         except:
             return 'did not check against the database'
     else:
