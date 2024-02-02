9dd78a0ef3d2ce5ae08dab2f83ce050891bec8b4
Dennis Lee <leedennis04@gmail.com>
2024-02-01 20:33:25 -0600

    updated server.py file

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, send_file
 import csv
 import requests
 import hashlib
