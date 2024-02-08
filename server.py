5bb5b2fca80a7c9e2ad465afeeab8d6a3ff72717
Dennis Lee <leedennis04@gmail.com>
2024-02-08 10:13:44 -0600

    updated workthree.html & server.py file

--- server.py
+++ server.py
@@ -142,8 +142,8 @@ def submit_correlation():
                 #formulaUsedThree = "Place holder for Formula Three"
                 formulaUsedThree = f"sns.heatmap(data = df_correlation.corr(), annot = True, fmt = '.2g', center = 0, cmap = 'coolwarm', linewidth = 1, linecolor = 'black')"
                 resultsTwo = "Place holder for results Two"
-                
-                return render_template(page_name, tables=[firstFive.to_html()], titles=[''], rowOne=rowOne, columnOne=columnOne, formulaUsed="First Formula Used:"+formulaUsed, formulaUsedTwo="Second Formula Used:"+formulaUsedTwo, formulaUsedThree=formulaUsedThree, resultsOne=[resultsOne.to_html()], resultsTwo=resultsTwo)
+
+                return render_template(page_name, tables=[firstFive.to_html()], titles=[''], rowOne=rowOne, columnOne=columnOne, formulaUsed="First Formula Used:"+formulaUsed, formulaUsedTwo="Second Formula Used:"+formulaUsedTwo, formulaUsedThree="Third Formula Used:"+formulaUsedThree, resultsOne=[resultsOne.to_html()], resultsTwo=resultsTwo)
             else:
                 return (str(page_name) + " Not Found")
         except RuntimeError as rte:
