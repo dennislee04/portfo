aad08ee0fefb8d895fa75336216a10ddce663c46
Dennis Lee <leedennis04@gmail.com>
2024-03-06 11:52:56 -0600

    updated workone.html file

--- server.py
+++ server.py
@@ -285,10 +285,10 @@ def submit_form2(data="Lets see if you have a Strong Password!"):
             #return {data}
             if (count):
                  # return render_template('workone.html', data=f"Your password: \'{pwd[0]}\' was FOUND: {count} times! You should change your password!")
-                return render_template('workone.html', data=f"Your password \'{pwd}\' was FOUND: {count} times. You should change your password!")
+                return render_template('workone.html', data1=f"Your password \'{pwd}\' was FOUND: {count} times. You should change your password!")
             else:
                  # return render_template('workone.html', data=f"Your password: \'{pwd[0]}\' was NOT FOUND. Carry on!")
-                return render_template('workone.html', data=f"Your password \'{pwd}\' was NOT FOUND. Carry on!")
+                return render_template('workone.html', data1=f"Your password \'{pwd}\' was NOT FOUND. Carry on!")
         except:
             return 'did not check against the database'
     else:
